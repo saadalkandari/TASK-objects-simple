80c3dbc89a2b3d2b5ae4bf6c4cbca6303936b3e0
Your Name <you@example.com>
2022-01-11 21:33:16 +0300

    done

--- index.js
+++ index.js
@@ -12,6 +12,29 @@
  */
 
 // add the movies variables here ....
+ const movie1 = {
+     id: 1,
+     name: "Avatar", 
+     image: "http://www.impawards.com/2009/posters/avatar_ver5.jpg",
+     rating: 9, 
+}; 
+const movie2 = {
+    id: 2,
+    name: "The Unforgivable",    
+    image:"https://images.fandango.com/ImageRenderer/820/0/redesign/static/img/default_poster.png/0/images/masterrepository/fandango/226423/UNFORGIVABLE_Main_Vertical_27x40_RGB_EN-GB.jpg ",
+    rating: 8, 
+
+};
+const movie3 = {
+    id: 3, 
+    name: "Morbius",
+    image: "https://media.news9live.com/h-upload/2021/12/06/89225-morbius.jpg",
+    rating: 7.5, 
+
+};
+console.log(movie1);
+console.log(movie2);
+console.log(movie3);
 
 /** Task 2:
  * Create an array `movies`
@@ -29,6 +52,32 @@
  *    },
  * ]
  */
+const movies = [
+    {
+         id: 1,
+         name: "Avatar", 
+         image: "https://i.pinimg.com/originals/e8/e7/a2/e8e7a2ab4bbd0a540d21dc52da2a540f.jpg",
+        rating: 9, 
+    },
+
+    {
+        id: 2,
+        name: "The Unforgivable",    
+        image: "https://images.fandango.com/ImageRenderer/820/0/redesign/static/img/default_poster.png/0/images/masterrepository/fandango/226423/UNFORGIVABLE_Main_Vertical_27x40_RGB_EN-GB.jpg",
+       rating: 8, 
+    
+    },
+
+    {
+        id: 3, 
+        name: "Morbius",
+        image: "https://media.news9live.com/h-upload/2021/12/06/89225-morbius.jpg",
+        rating: 7.5, 
+    },
+
+
+];
+console.log(movies);
 
 /** TASK 3:
  * Create a new file call it `movies.json`
